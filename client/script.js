5aceb7ce7f934fee1251f6ee10f375997cae6978
salma <mrabet.salma@ensi-uma.tn>
2023-01-20 12:49:22 +0100

    error

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server => bot's response
 
-  const response = await fetch ('https://smarty.onrender.com',{
+  const response = await fetch ('https://smarty.onrender.com/',{
     method:'POST',
     headers:{
       'content-Type':'application/json'
