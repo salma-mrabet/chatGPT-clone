bee0f8d676dcd89d25c7b227ab4b4575d0b4461b
salma <mrabet.salma@ensi-uma.tn>
2023-01-20 12:39:19 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server => bot's response
 
-  const response = await fetch ('http://localhost:5000',{
+  const response = await fetch ('https://smarty.onrender.com',{
     method:'POST',
     headers:{
       'content-Type':'application/json'
